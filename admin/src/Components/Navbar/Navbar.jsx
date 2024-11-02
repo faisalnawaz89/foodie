14905c119ebbda2a7e8f613be01f0768f6802fcb
Faisal Nawaz <isfaisalnawaz@gmail.com>
2024-11-02 17:08:00 +0530

    add navbar css code

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img className="logo" src={assets.logo} alt="" />
-        <img className="profile" src={assets.profile_image} alt="" />
+        <button className="logout" onClick={()=>{localStorage.removeItem('token');window.location.replace('/login')}}>Logout</button>
     </div>
   )
 }

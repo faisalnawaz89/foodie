c52cd026e933a1da7dd38e22e054d2c55ee9e86e
Faisal Nawaz <isfaisalnawaz@gmail.com>
2024-11-02 14:36:38 +0530

    add some change on entire project

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //Placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = 'http://localhost:5173'
+    const frontend_url = 'https://foodie-app-tau-ten.vercel.app'
 
     try {
         const newOrder = new orderModel({
